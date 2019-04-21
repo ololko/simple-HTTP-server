25febaab15e4a58486128922ed8ee3fa0b09f1f6
Luppolo <ololko@github.com>
2019-04-21 22:16:43 +0300

    Parsing querry

--- api/server.go
+++ api/server.go
@@ -7,6 +7,8 @@ import (
   "net/http"
   "myDecoder"
   "eventStructure"
+  "strings"
+  "strconv"
   //"net/url"
   "golang.org/x/net/context"
   firebase "firebase.google.com/go"
@@ -29,28 +31,60 @@ func main() {
     }
     defer client.Close()
 
+
 //      SERVER SIDE
+    maxTime := 0
+    minTime := 0
+
     http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request){
       if r.Method == "GET"{
-          fmt.Fprintf(w, "NOT implemented")
-          fmt.Println(r.URL.RawQuery)
-
-        } else if r.Method == "POST"{
-
-          var newEvent eventStructure.Event
-          newEvent = myDecoder.Decode(r)
-          fmt.Fprintf(w, newEvent.Type)
-
-          _, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
-              "Count"     : newEvent.Count,
-              "Type"      : newEvent.Type,
-              "Timestamp" : newEvent.Timestamp,
-          })
-          if err != nil {
-              log.Fatalf("Failed adding alovelace: %v", err)
+          var querries = strings.Split(r.URL.RawQuery,"&")
+          
+          from := minTime
+          to := maxTime
+          //count := 0
+          var searchedEvent string
+          for i := 0; i < len(querries); i++ {
+            if strings.Contains(querries[i],"from="){
+              from,err = strconv.Atoi(querries[i][5:])
+              if err != nil {
+                log.Fatal("Couldn't convert string to integer!")
+              }
+            }
+            if strings.Contains(querries[i],"to="){
+              to,err = strconv.Atoi(querries[i][3:])
+              if err != nil {
+                log.Fatal("Couldn't convert string to integer!")
+              }
+            }
+            if strings.Contains(querries[i],"type="){
+              searchedEvent = querries[i][5:]
+            }
           }
 
+          fmt.Println(from)
+          fmt.Println(to)
+          fmt.Println(searchedEvent)
 
+        } else if r.Method == "POST"{
+            var newEvent eventStructure.Event
+            newEvent = myDecoder.Decode(r)
+
+            if newEvent.Timestamp > maxTime {
+              maxTime = newEvent.Timestamp
+            }
+            if newEvent.Timestamp < minTime {
+              minTime = newEvent.Timestamp
+            }
+
+            _, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+                "Count"     : newEvent.Count,
+                "Type"      : newEvent.Type,
+                "Timestamp" : newEvent.Timestamp,
+            })
+            if err != nil {
+                log.Fatalf("Failed adding alovelace: %v", err)
+            }
         } else {
           fmt.Fprintf(w, "hod err not supported method")
 
